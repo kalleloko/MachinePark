d2bba33478411c45e8fc58088d1e7a56ce57d00f
Kalle Lindqvist <kallelin@gmail.com>
2026-03-10 09:56:16 +0100

    add addMachine modal

---
+++ MachinePark/MachinePark/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿window.showModal = (id) => {
+    const modal = new bootstrap.Modal(document.getElementById(id));
+    modal.show();
+}
+
+window.hideModal = (id) => {
+    const el = document.getElementById(id)
+    const modal = bootstrap.Modal.getInstance(el)
+    modal.hide()
+}
