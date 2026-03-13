a88edb962b617fd82a26c1e437eb68f33a2ffefc
Kalle Lindqvist <kallelin@gmail.com>
2026-03-13 09:52:12 +0100

    add some nice styles

--- MachinePark/MachinePark.Shared/Models/AddMachine.cs
+++ MachinePark/MachinePark.Shared/Models/AddMachine.cs
@@ -8,7 +8,7 @@ public class AddMachine
     [MinLength(2, ErrorMessage = "Name must be at least 2 characters.")]
     [MaxLength(15, ErrorMessage = "Name can’t be longer than 15 characters.")]
     public string Name { get; set; } = string.Empty;
-    public bool IsOnline { get; set; } = false;
+    public bool IsOnline { get; set; } = true;
 
     [MaxLength(15, ErrorMessage = "Data can’t be longer than 15 characters.")]
     public string LastData { get; set; } = string.Empty;
